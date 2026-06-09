72583254f4b170c4eb22ae302e97adfe47a09f39
Reegan <reegansuman@gmail.com>
2026-06-09 12:15:54 +0530

    fix

--- out/build/x64-Debug/raylib_build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/raylib/raylib/src
-# Build directory: D:/Main_projects/walkie_talkie/out/build/x64-Debug/raylib_build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
