f6692f27a68575bd2a9aed78958d921a7538ede7
Reegan <reegansuman@gmail.com>
2026-06-09 14:15:29 +0530

    camera FOV fixed

--- worldscene.cpp
+++ worldscene.cpp
@@ -6,7 +6,7 @@ WorldScene::WorldScene()
     camera.position = { 0.0f, 10.0f, 10.0f };
     camera.target = { 0.0f, 0.0f, 0.0f };
     camera.up = { 0.0f, 1.0f, 0.0f };
-    camera.fovy = 95.0f;
+    camera.fovy = 65.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 }
 void WorldScene::update(float dt)
@@ -35,7 +35,7 @@ void WorldScene::draw()
     BeginMode3D(camera);
 
     DrawPlane({ 0,0,0 }, { 50,50 }, WHITE);
-    DrawCube({ 5,1,5 }, 1, 1, 1, RED);
+    //DrawCube({ 5,1,5 }, 1, 1, 1, RED);
     player.draw();
 
     EndMode3D();
